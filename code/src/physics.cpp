9369af0b71921ff99d9ecde87316db7ed2ccc86c
Fran <Aguilo>
2020-04-07 17:30:16 +0200

    init 2

---
+++ code/src/physics.cpp
@@ -0,0 +1,74 @@
+#include <imgui\imgui.h>
+#include <imgui\imgui_impl_sdl_gl3.h>
+#include <glm\glm.hpp>
+
+//Exemple
+extern void Exemple_GUI();
+extern void Exemple_PhysicsInit();
+extern void Exemple_PhysicsUpdate(float dt);
+extern void Exemple_PhysicsCleanup();
+
+bool show_test_window = false;
+void GUI() {
+	bool show = true;
+	ImGui::Begin("Physics Parameters", &show, 0);
+
+	{	
+		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);//FrameRate
+		Exemple_GUI();
+	}
+	
+	ImGui::End();
+}
+
+namespace LilSpheres {
+	extern const int maxParticles;
+	extern int firstParticleIdx;
+	extern int particleCount;
+	extern void updateParticles(int startIdx, int count, float* array_data);
+}
+
+namespace {
+	static struct PhysParams {
+		float min = 0.f;
+		float max = 10.f;
+	} p_pars;
+
+	static struct ParticleSystem {
+		glm::vec3 *position;
+		int numParticles;
+	} myPS;
+}
+
+////////////////////
+
+void MyPhysicsInit() {
+	
+}
+
+void MyPhysicsUpdate(float dt) {
+	
+}
+
+void MyPhysicsCleanup() {
+	//delete[] s_PS.position;
+}
+
+
+
+
+
+
+/////////////////////////////////
+
+void PhysicsInit() {
+	
+}
+
+void PhysicsUpdate(float dt) {
+	
+}
+
+void PhysicsCleanup() {
+	
+}
