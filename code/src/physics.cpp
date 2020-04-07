835f2aa2d705151dab440a340ad2ba4a0f31131e
Fran <Aguilo>
2020-04-07 18:44:46 +0200

    aux

--- code/src/physics.cpp
+++ code/src/physics.cpp
@@ -2,22 +2,24 @@
 #include <imgui\imgui_impl_sdl_gl3.h>
 #include <glm\glm.hpp>
 
+
 //Exemple
 extern void Exemple_GUI();
 extern void Exemple_PhysicsInit();
 extern void Exemple_PhysicsUpdate(float dt);
 extern void Exemple_PhysicsCleanup();
 
+
 bool show_test_window = false;
 void GUI() {
 	bool show = true;
 	ImGui::Begin("Physics Parameters", &show, 0);
 
-	{	
+	{
 		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);//FrameRate
 		Exemple_GUI();
 	}
-	
+
 	ImGui::End();
 }
 
@@ -38,20 +40,41 @@ namespace {
 		glm::vec3 *position;
 		int numParticles;
 	} myPS;
+
+	static struct MeshPoint {
+		glm::vec3 position;
+		glm::vec3 totalForce; //total
+		glm::vec3 structuralForce; //horitzontals i verticals
+		glm::vec3 shearForce; //diagonals
+		glm::vec3 bendingForce; //contaria?
+		float mass;
+
+		MeshPoint *rightPoint;
+		MeshPoint *leftPoint;
+		MeshPoint *upPoint;
+		MeshPoint *downPoint;
+	};
+
+	static struct ParticleMesh {
+		MeshPoint *points;
+		float kEslatic = 1;
+		float kDump = 1;
+
+	} myPM;
 }
 
 ////////////////////
 
 void MyPhysicsInit() {
-	
+
 }
 
 void MyPhysicsUpdate(float dt) {
-	
+
 }
 
 void MyPhysicsCleanup() {
-	//delete[] s_PS.position;
+	delete[] myPM.points;
 }
 
 
@@ -62,13 +85,13 @@ void MyPhysicsCleanup() {
 /////////////////////////////////
 
 void PhysicsInit() {
-	
+
 }
 
 void PhysicsUpdate(float dt) {
-	
+
 }
 
 void PhysicsCleanup() {
-	
+
 }
