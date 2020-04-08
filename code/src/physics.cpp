62d89fd78f15504d24569b0adfdeb9409760bbbe
Fran <Aguilo>
2020-04-08 15:11:26 +0200

    inici verlet
    no va pero vaig dinar i vull deixar push

--- code/src/physics.cpp
+++ code/src/physics.cpp
@@ -56,8 +56,12 @@ namespace {
 	};
 
 	static struct ParticleMesh {
-		MeshPoint **points;
-		glm::vec3 *pointsPositions;
+
+		MeshPoint **previusPoints;//punts anteriors
+		MeshPoint **actualPoints;//punts actuals
+		MeshPoint **nextPoints;//punts següents
+
+		glm::vec3 *pointsPositions;//?
 		float kEslatic = 1;
 		float kDump = 1;
 
@@ -71,18 +75,18 @@ void MyPhysicsInit() {
 
 	ClothMesh::setupClothMesh();
 
-	myPM.points = new MeshPoint*[18];
+	myPM.actualPoints = new MeshPoint*[18];
 	for (int i = 0; i < 18; i++) {
-		myPM.points[i] = new MeshPoint[14];
+		myPM.actualPoints[i] = new MeshPoint[14];
 	}
 	myPM.pointsPositions = new glm::vec3[14*18];
 
 	for (int i = 0; i < 18; i++) {
 		for (int j = 0; j < 14; j++) {
-			myPM.points[i][j].position.x = i * 0.5f - 17 * 0.5f * 0.5f;
-			myPM.points[i][j].position.y = 9;
-			myPM.points[i][j].position.z = j * 0.5f - 13 * 0.5f * 0.5f;
-			myPM.pointsPositions[i*14 + j] = glm::vec3(myPM.points[i][j].position.x, myPM.points[i][j].position.y, myPM.points[i][j].position.z);
+			myPM.actualPoints[i][j].position.x = i * 0.5f - 17 * 0.5f * 0.5f;
+			myPM.actualPoints[i][j].position.y = 9;
+			myPM.actualPoints[i][j].position.z = j * 0.5f - 13 * 0.5f * 0.5f;
+			myPM.pointsPositions[i*14 + j] = glm::vec3(myPM.actualPoints[i][j].position.x, myPM.actualPoints[i][j].position.y, myPM.actualPoints[i][j].position.z);
 			
 		}
 	}
@@ -107,16 +111,30 @@ void MyPhysicsInit() {
 
 void MyPhysicsUpdate(float dt) {
 	
+	//verlet
+	for (int i = 0; i < 18; i++) {
+		for (int j = 0; j < 14; j++) {
+			//caluclar força total
+
+
+			//canviar posicions
+			//posicionSiguiente=posicionActual + (posicionActual - posicionAnterior) + (f/m)* dt^2
+			//FALTA dt QUE NO SE QUE ES
+			//myPM.nextPoints[i][j].position = myPM.actualPoints[i][j].position + (myPM.actualPoints[i][j].position - myPM.previusPoints[i][j].position) + (myPM.actualPoints[i][j].totalForce / myPM.actualPoints[i][j].mass)/* * dt^2 */;
+
+		}
+	}
 
 
 
-	//ClothMesh::updateClothMesh(&(myPM.points[0][0].position.x));
+
 	
+	//ClothMesh::updateClothMesh(&(myPM.points[0][0].position.x));
 }
 
 void MyPhysicsCleanup() {
 	ClothMesh::cleanupClothMesh();
-	delete[] myPM.points;
+	delete[] myPM.actualPoints;
 }
 
 
