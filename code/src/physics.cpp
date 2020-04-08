8162765bdd1aa7dacbd28649077ca57c3cfa9c8b
Victor Blasal <victor.blas@enti.cat>
2020-04-08 17:34:01 +0200

    checkpoint

--- code/src/physics.cpp
+++ code/src/physics.cpp
@@ -114,16 +114,60 @@ void MyPhysicsInit() {
 			myPM.points[i][j].lastPosition = myPM.points[i][j].actualPosition;
 
 			myPM.points[i][j].velocity = glm::vec3(0,0,0); //velocitat inicial = 0
-			//myPM.points[i][j].lastVelocity = myPM.points[i][j].velocity;
-			myPM.points[i][j].totalForce = glm::vec3(0, myPM.gravity, 0); //força total inicial = gravetat
+			myPM.points[i][j].totalForce = glm::vec3(0, 0/*myPM.gravity*/, 0); //força total inicial = gravetat
 			myPM.points[i][j].shearForce = glm::vec3(0, 0, 0);
 			myPM.points[i][j].structuralForce = glm::vec3(0, 0, 0);
 		    myPM.points[i][j].bendingForce = glm::vec3(0, 0, 0);
 
+			//pointers laterals
+			if (i == 0) {
+				myPM.points[i][j].leftPoint = nullptr;
+				myPM.points[i][j].rightPoint = &myPM.points[i + 1][j];
+			}
+			else if (i == 17) {
+				myPM.points[i][j].leftPoint = &myPM.points[i - 1][j];
+				myPM.points[i][j].rightPoint = nullptr;
+			}
+			else {
+				myPM.points[i][j].leftPoint = &myPM.points[i-1][j];
+				myPM.points[i][j].rightPoint = &myPM.points[i+1][j];
+			}
 
-
+			//pointers verticals
+			if (j == 0) {
+				myPM.points[i][j].downPoint = nullptr;
+				myPM.points[i][j].upPoint = &myPM.points[i][j+1];
+			}
+			else if (j == 13) {
+				myPM.points[i][j].downPoint = &myPM.points[i][j-1];
+				myPM.points[i][j].upPoint = nullptr;
+			}
+			else {
+				myPM.points[i][j].downPoint = &myPM.points[i][j-1];
+				myPM.points[i][j].upPoint = &myPM.points[i][j+1];
+			}
 		}
 	}
+	for (int i = 0; i < 18; i++) {
+		for (int j = 0; j < 14; j++) {
+			//Longituds
+
+			if (myPM.points[i][j].leftPoint != nullptr) {
+				myPM.points[i][j].LtoLeft = glm::length<float>(myPM.points[i][j].actualPosition- myPM.points[i-1][j].actualPosition);
+			}
+			if (myPM.points[i][j].rightPoint != nullptr) {
+				myPM.points[i][j].LtoRight = glm::length<float>(myPM.points[i][j].actualPosition - myPM.points[i + 1][j].actualPosition);
+			}
+			if (myPM.points[i][j].downPoint != nullptr) {
+				myPM.points[i][j].LtoDown = glm::length<float>(myPM.points[i][j].actualPosition - myPM.points[i][j-1].actualPosition);
+			}
+			if (myPM.points[i][j].upPoint != nullptr) {
+				myPM.points[i][j].LtoUp = glm::length<float>(myPM.points[i][j].actualPosition - myPM.points[i][j+1].actualPosition);
+			}
+			
+		}
+	}
+
 	updateMyPMPointsPositions();
 	ClothMesh::updateClothMesh(&(myPM.pointsPositions[0].x));
 
